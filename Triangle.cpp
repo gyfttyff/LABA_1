c361a946563c7b947c5a1f8b376ac85ebb7f7031
Danil <9139316075mok@gmail.com>
2026-03-21 21:10:13 +0700

    Добавлено описание каждой функции в проекте и проверка на существование треугольника

--- Triangle.cpp
+++ Triangle.cpp
@@ -4,19 +4,26 @@
 
 using namespace std;
 
-int perimeter(float a, float b, float c)
+string test_of_exist (float a, float b, float c) // Проверка на существование треугольника по неравенствам треугольника
+{
+	string answ = "No... It's not a Triangle";
+	if (a < b + c && c < b + a && b < c + a) answ = "Yes! It's a Triangle";
+	return answ;
+}
+
+int perimeter(float a, float b, float c) // Периметр треугольника
 {
 	return a+b+c;
 }
 
-float square_geron(float a, float b, float c)
+float square_geron(float a, float b, float c) // Площадь треугольника по формуле герона
 {
 	float polper = perimeter(a, b, c) / 2.0f;
 	double s = sqrt(polper * (polper - a) * (polper - b) * (polper - c)); 
        	return s;	
 }
 
-string equisceles_check (float a, float b, float c)
+string equisceles_check (float a, float b, float c) // Проверка на равнобедренность 
 {
 	string answ = "NO";
         if (a - b == 0 || a - c == 0 || c - b == 0) answ = "YES";
@@ -26,16 +33,18 @@ string equisceles_check (float a, float b, float c)
 int main()
 {
 	float a = 9, b = 3, c = 2;
-	cout << "" << endl;
+       	cout << "" << endl;	
 	cout << "START PROGRAM:" << endl;
 	cout << "" << endl;
 	cout << "Sides of the triangle: " << a << ", " << b << ", " << c << endl;
 	cout << "" << endl;
-	cout << "===PERIMETER:===	" << perimeter(a, b, c) << endl;
+	cout << "===TEST EXISTENCE OF TRIANGLE===   " << test_of_exist(a, b, c) << endl;
 	cout << "" << endl;
-	cout << "===SQUARE:===        " << square_geron(a, b, c) << endl;
+	cout << "===PERIMETER:===   " << perimeter(a, b, c) << endl;
 	cout << "" << endl;
-	cout << "===EQUISCELES CHECK:===        " <<  equisceles_check(a, b, c) << endl;
+	cout << "===SQUARE:===   " << square_geron(a, b, c) << endl;
+	cout << "" << endl;
+	cout << "===EQUISCELES CHECK:===   " <<  equisceles_check(a, b, c) << endl;
 	cout << "" << endl;
 	return 0;
 }
