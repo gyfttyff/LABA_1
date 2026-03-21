eeeff3399f2622b0c3750dfa86332210399bb59f
Danil <9139316075mok@gmail.com>
2026-03-21 20:22:18 +0700

    Доделана функция площади треугольника

--- Triangle.cpp
+++ Triangle.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-// #include <>
+#include <cmath>
 
 using namespace std;
 
@@ -11,14 +11,15 @@ int perimeter(float a, float b, float c)
 float square_geron(float a, float b, float c)
 {
 	float polper = perimeter(a, b, c) / 2.0f;
-	float s = 0; 
-       	return 0;	
+	double s = sqrt(polper * (polper - a) * (polper - b) * (polper - c)); 
+       	return s;	
 }
 
 int main()
 {
 	float a = 2, b = 3, c = 2;
-	cout << "===PERIMETER:=== " << perimeter(a, b, c) << endl;
-	cout << "===SQUARE:===" << square_geron(a, b, c) << endl;
+	cout << "===PERIMETER:===	" << perimeter(a, b, c) << endl;
+	cout << "" << endl;
+	cout << "===SQUARE:===        " << square_geron(a, b, c) << endl;
 	return 0;
 }
