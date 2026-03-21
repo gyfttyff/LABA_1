06da28f2ee35d84d8a0a62ebc43f4dff0a9bdd3e
Danil <9139316075mok@gmail.com>
2026-03-21 20:05:49 +0700

    библиотеки math не существует

--- Triangle.cpp
+++ Triangle.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+// #include <>
 
 using namespace std;
 
@@ -9,14 +10,15 @@ int perimeter(float a, float b, float c)
 
 float square_geron(float a, float b, float c)
 {
-	float p = perimeter(a, b, c) / 2.0f;
-	return p;
+	float polper = perimeter(a, b, c) / 2.0f;
+	float s = 0; 
+       	return 0;	
 }
 
 int main()
 {
 	float a = 2, b = 3, c = 2;
 	cout << "===PERIMETER:=== " << perimeter(a, b, c) << endl;
-	cout << "===HALF-PERIMETER:===" << square_geron(a, b, c) << endl;
+	cout << "===SQUARE:===" << square_geron(a, b, c) << endl;
 	return 0;
 }
