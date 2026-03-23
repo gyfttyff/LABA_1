2f1676c51adc9e2c4f74c93636cdf032398de688
9139316075mok@gmail.com <9139316075mok@gmail.com>
2026-03-23 09:13:09 +0700

    Добавлен ввод сторон с клавиатуры

--- Triangle.cpp
+++ Triangle.cpp
@@ -32,8 +32,8 @@ string equisceles_check (float a, float b, float c) // Проверка на р�
 
 int main()
 {
-	float a = 4, b = 3, c = 2;
-       	cout << "" << endl;	
+	float a, b, c;
+       	cin >> a >> b >> c;	
 	cout << "START PROGRAM:" << endl;
 	cout << "" << endl;
 	cout << "Sides of the triangle: " << a << ", " << b << ", " << c << endl;
